f1a88163ca249b9608ed99324b32d51d33387f03
michael-gif <53706169+michael-gif@users.noreply.github.com>
2019-12-02 22:36:45 +0000

    Update Virtual_CPU.py

--- Virtual_CPU.py
+++ Virtual_CPU.py
@@ -3,10 +3,9 @@ instructions = []
 registers = [0,0,0,0,0,0]
 memory = [1,0,0,0,0,0,0,0]
 instructioncount = 0
-accumulator = 0
 app = tk.Tk()
 app.title("Virtual CPU")
-app.geometry('1365x525')
+app.geometry('1255x525')
 app.configure(background='gray25')
 app.iconbitmap(r'cpuicon.ico')
 app.resizable(0,0)
@@ -38,6 +37,34 @@ def clearprevious():
     listbox1.delete('end')
     instructioncount -= 1
     instructions.pop(len(instructions)-1)
+
+def clearoutput():
+    listbox2.delete(0,'end')
+
+def clearprevoutput():
+    listbox2.delete('end')
+
+def clearregisters():
+    global registers
+    registervalue1.set("0: " + str(0))
+    registervalue2.set("1: " + str(0))
+    registervalue3.set("2: " + str(0))
+    registervalue4.set("3: " + str(0))
+    registervalue5.set("4: " + str(0))
+    registervalue6.set("5: " + str(0))
+    registers = [0,0,0,0,0,0]
+
+def clearmemory():
+    global memory
+    memoryvalue1.set("0: " + str(0))
+    memoryvalue2.set("1: " + str(0))
+    memoryvalue3.set("2: " + str(0))
+    memoryvalue4.set("3: " + str(0))
+    memoryvalue5.set("4: " + str(0))
+    memoryvalue6.set("5: " + str(0))
+    memoryvalue7.set("6: " + str(0))
+    memoryvalue8.set("7: " + str(0))
+    memory = [0,0,0,0,0,0,0,0]
 
 def decodebinary(value):
     total = 0
@@ -73,7 +100,7 @@ def changelabelvalue():
         print("Error:entry type invalid")
 
 def startprogram():
-    global registers, memory, accumulator
+    global registers, memory
     x = 0
     reg1 = 0
     reg2 = 0
@@ -85,7 +112,6 @@ def startprogram():
             command = instructions[x+2]
             address = decodebinary(command)
             memory[address] = data
-            updateregisters()
             x += 3            
         elif instructions[x] == "0001":
             listbox2.insert('end',"Executing " + str(x+1) + ": " + instructions[x] + "\n")
@@ -95,24 +121,22 @@ def startprogram():
             command = instructions[x+2]
             register = decodebinary(command)
             registers[register] = data
-            updateregisters()
             x += 3
         elif instructions[x] == "0010":
             listbox2.insert('end',"Executing " + str(x+1) + ": " + instructions[x] + "\n")
             command = instructions[x+1]
-            storelocation = decodebinary(command)
-            memory[storelocation] = accumulator
-            updateregisters()
-            x += 2
+            storefrom = decodebinary(command)
+            command = instructions[x+2]
+            storeto = decodebinary(command)
+            memory[storeto] = registers[storefrom]
+            x += 3
         elif instructions[x] == "0011":
             listbox2.insert('end',"Executing " + str(x+1) + ": " + instructions[x] + "\n")
             command = instructions[x+1]
             reg1 = decodebinary(command)
             command = instructions[x+2]
             reg2 = decodebinary(command)
-            accumulator = registers[reg1] + registers[reg2]
-            accumulatorvalue.set(accumulator)
-            updateregisters()
+            registers[reg1] = registers[reg1] + registers[reg2]
             x += 3
         elif instructions[x] == "0100":
             listbox2.insert('end',"Executing " + str(x+1) + ": " + instructions[x] + "\n")
@@ -120,8 +144,7 @@ def startprogram():
             reg1 = decodebinary(command)
             command = instructions[x+2]
             reg2 = decodebinary(command)
-            accumulator = registers[reg1] - registers[reg2]
-            accumulatorvalue.set(accumulator)
+            registers[reg1] = registers[reg1] - registers[reg2]
             x += 3
         elif instructions[x] == "0101":
             listbox2.insert('end',"Executing " + str(x+1) + ": " + instructions[x] + "\n")
@@ -129,8 +152,7 @@ def startprogram():
             reg1 = decodebinary(command)
             command = instructions[x+2]
             reg2 = decodebinary(command)
-            accumulator = registers[reg1] * registers[reg2]
-            accumulatorvalue.set(accumulator)
+            registers[reg1] = registers[reg1] * registers[reg2]
             x += 3
         elif instructions[x] == "0110":
             listbox2.insert('end',"Executing " + str(x+1) + ": " + instructions[x] + "\n")
@@ -138,12 +160,12 @@ def startprogram():
             reg1 = decodebinary(command)
             command = instructions[x+2]
             reg2 = decodebinary(command)
-            accumulator = registers[reg1] / registers[reg2]
-            accumulatorvalue.set(accumulator)
+            registers[reg1] = registers[reg1] / registers[reg2]
             x += 3
         else:
             print("Error:entry invalid")
             x = len(instructions)
+        updateregisters()
 
 #sequence box
 labeltext = tk.StringVar(app)
@@ -190,58 +212,57 @@ memorylabel6 = tk.Label(app,bg="black",fg='white',textvariable=memoryvalue6,anch
 memorylabel7 = tk.Label(app,bg="black",fg='white',textvariable=memoryvalue7,anchor='w').place(x=440,y=210,width=100)
 memorylabel8 = tk.Label(app,bg="black",fg='white',textvariable=memoryvalue8,anchor='w').place(x=440,y=240,width=100)
 
-#ALU box
-accumulatorvalue = tk.StringVar(app)
-accumulatorlabel = tk.Label(app,bg="black",fg='white',textvariable=accumulatorvalue).place(x=550,y=30,width=100)
-
 #titles
 tk.Label(app,text="Sequence:",bg='grey25',fg='snow').place(x=10,y=5)
 tk.Label(app,text="Instruction:",bg='grey25',fg='snow').place(x=10,y=285)
 tk.Label(app,text="Program Counter Output:",bg='grey25',fg='snow').place(x=170,y=5)
 tk.Label(app,text="Register Values:",bg='grey25',fg='snow').place(x=330,y=5)
 tk.Label(app,text="Memory Values:",bg='grey25',fg='snow').place(x=440,y=5)
-tk.Label(app,text="ALU Output:",bg='grey25',fg='snow').place(x=550,y=5)
 
 #Guide text
-tk.Label(app,text="Guide:",bg='grey25',fg='snow').place(x=660,y=5)
+tk.Label(app,text="Guide:",bg='grey25',fg='snow').place(x=550,y=5)
 
-tk.Label(app,text="0000",bg='grey25',fg='snow').place(x=660,y=25)
-tk.Label(app,text="Function: Sets memory values.",bg='grey25',fg='snow').place(x=660,y=45)
-tk.Label(app,text="Usage: Enter 0000, followed by the binary data followed by the binary address of the desired memory cell.",bg='grey25',fg='snow').place(x=660,y=65)
+tk.Label(app,text="0000",bg='grey25',fg='snow').place(x=550,y=25)
+tk.Label(app,text="Function: Sets memory values.",bg='grey25',fg='snow').place(x=550,y=45)
+tk.Label(app,text="Usage: Enter 0000, followed by the binary data followed by the binary address of the desired memory cell.",bg='grey25',fg='snow').place(x=550,y=65)
 
-tk.Label(app,text="0001",bg='grey25',fg='snow').place(x=660,y=95)
-tk.Label(app,text="Function: Loads a value from memory to a register.",bg='grey25',fg='snow').place(x=660,y=115)
-tk.Label(app,text="Usage: Enter 0001, followed by the binary address of the data in memory followed by the binary address of the desired register.",bg='grey25',fg='snow').place(x=660,y=135)
+tk.Label(app,text="0001",bg='grey25',fg='snow').place(x=550,y=95)
+tk.Label(app,text="Function: Loads a value from memory to a register.",bg='grey25',fg='snow').place(x=550,y=115)
+tk.Label(app,text="Usage: Enter 0001, followed by the binary address of the data in memory followed by the binary address of the desired register.",bg='grey25',fg='snow').place(x=550,y=135)
 
-tk.Label(app,text="0010",bg='grey25',fg='snow').place(x=660,y=165)
-tk.Label(app,text="Function: Stores the result in memory.",bg='grey25',fg='snow').place(x=660,y=185)
-tk.Label(app,text="Usage: Enter 0011, then the address of the memory where the data willl be stored.",bg='grey25',fg='snow').place(x=660,y=205)
+tk.Label(app,text="0010",bg='grey25',fg='snow').place(x=550,y=165)
+tk.Label(app,text="Function: Stores the data in a register into memory.",bg='grey25',fg='snow').place(x=550,y=185)
+tk.Label(app,text="Usage: Enter 0011, enter the register number to be stored, then the address of the memory where the data will be stored.",bg='grey25',fg='snow').place(x=550,y=205)
 
-tk.Label(app,text="0011",bg='grey25',fg='snow').place(x=660,y=235)
-tk.Label(app,text="Function: Adds two registers.",bg='grey25',fg='snow').place(x=660,y=255)
-tk.Label(app,text="Usage: Enter 0010, then the register number containing the first number, then the register number containing the second number.",bg='grey25',fg='snow').place(x=660,y=275)
+tk.Label(app,text="0011",bg='grey25',fg='snow').place(x=550,y=235)
+tk.Label(app,text="Function: Adds two registers.",bg='grey25',fg='snow').place(x=550,y=255)
+tk.Label(app,text="Usage: Enter 0010, then the register number containing the first number, then the register number containing the second number.",bg='grey25',fg='snow').place(x=550,y=275)
 
-tk.Label(app,text="0100",bg='grey25',fg='snow').place(x=660,y=305)
-tk.Label(app,text="Function: Subtracts two registers.",bg='grey25',fg='snow').place(x=660,y=325)
-tk.Label(app,text="Usage: Enter 0100, then the register number containing the first number, then the register number containing the second number.",bg='grey25',fg='snow').place(x=660,y=345)
+tk.Label(app,text="0100",bg='grey25',fg='snow').place(x=550,y=305)
+tk.Label(app,text="Function: Subtracts two registers.",bg='grey25',fg='snow').place(x=550,y=325)
+tk.Label(app,text="Usage: Enter 0100, then the register number containing the first number, then the register number containing the second number.",bg='grey25',fg='snow').place(x=550,y=345)
 
-tk.Label(app,text="0101",bg='grey25',fg='snow').place(x=660,y=375)
-tk.Label(app,text="Function: Multiplies two registers.",bg='grey25',fg='snow').place(x=660,y=395)
-tk.Label(app,text="Usage: Enter 0101, then the register number containing the first number, then the register number containing the second number.",bg='grey25',fg='snow').place(x=660,y=415)
+tk.Label(app,text="0101",bg='grey25',fg='snow').place(x=550,y=375)
+tk.Label(app,text="Function: Multiplies two registers.",bg='grey25',fg='snow').place(x=550,y=395)
+tk.Label(app,text="Usage: Enter 0101, then the register number containing the first number, then the register number containing the second number.",bg='grey25',fg='snow').place(x=550,y=415)
 
-tk.Label(app,text="0110",bg='grey25',fg='snow').place(x=660,y=445)
-tk.Label(app,text="Function: Divides two registers.",bg='grey25',fg='snow').place(x=660,y=465)
-tk.Label(app,text="Usage: Enter 0110, then the register number containing the first number, then the register number containing the second number.",bg='grey25',fg='snow').place(x=660,y=485)
+tk.Label(app,text="0110",bg='grey25',fg='snow').place(x=550,y=445)
+tk.Label(app,text="Function: Divides two registers.",bg='grey25',fg='snow').place(x=550,y=465)
+tk.Label(app,text="Usage: Enter 0110, then the register number containing the first number, then the register number containing the second number.",bg='grey25',fg='snow').place(x=550,y=485)
 
 #instruction input box
 entryvalue = tk.StringVar(app)
 entry1 = tk.Entry(app,textvariable=entryvalue).place(x=75,y=285,width=85)
 
 #buttons
-button = tk.Button(app,text="Add Instruction",bg='grey40',command=changelabelvalue).place(x=10,y=310,width=150)
+button1 = tk.Button(app,text="Add Instruction",bg='grey40',command=changelabelvalue).place(x=10,y=310,width=150)
 button2 = tk.Button(app,text="Run Program",bg='grey40',command=startprogram).place(x=170,y=310,width=150)
 button3 = tk.Button(app,text="Clear Sequence",bg='grey40',command=clearsequence).place(x=10,y=340,width = 150)
-button3 = tk.Button(app,text="Clear Previous",bg='grey40',command=clearprevious).place(x=10,y=370,width = 150)
+button4 = tk.Button(app,text="Clear Previous",bg='grey40',command=clearprevious).place(x=10,y=370,width = 150)
+button5 = tk.Button(app,text='Clear Output',bg='grey40',command=clearoutput).place(x=170,y=340,width=150)
+button6 = tk.Button(app,text='Clear Prev Output',bg='grey40',command=clearprevoutput).place(x=170,y=370,width=150)
+button7 = tk.Button(app,text='Clear Registers',bg='grey40',command=clearregisters).place(x=330,y=310,width=100)
+button8 = tk.Button(app,text='Clear Memory',bg='grey40',command=clearmemory).place(x=440,y=310,width=100)
 
 #set intial register and memory values
 updateregisters()
